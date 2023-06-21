28432643b341ab346934765a25a4839ee8a66af6
firstbroken <renanferreirasilva14@gmail.com>
2023-06-21 16:45:48 -0300

    Projeto MVC

---
+++ CadastroClientes/CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
